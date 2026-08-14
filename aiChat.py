00626527648d2ec21db075dd96e2a5099ff07304
hukel <huke.leander@gmail.com>
2026-08-14 10:00:31 -0700

    history controls take 4.1 added debug print more

--- aiChat.py
+++ aiChat.py
@@ -183,7 +183,7 @@ with st.bottom:
                 h_amount
                 len(st.session_state.messages)
                 st.session_state.messages[h_amount+1]
-                response = client.chat.completions.create(model=MODEL, messages=st.session_state.messages[h_amount:len(st.session_state.messages)+1])
+                #response = client.chat.completions.create(model=MODEL, messages=st.session_state.messages[h_amount:len(st.session_state.messages)+1])
             else:
                 response = client.chat.completions.create(model=MODEL, messages=messages)
             st.session_state.messages.append({"role":"assistant", "content":response.choices[0].message.content})
